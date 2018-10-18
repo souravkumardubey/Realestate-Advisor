ac2dbd825ae6ec19a7d116af2df398344697e8a8
Shubham Dubey <30700289+dubeysk@users.noreply.github.com>
2018-10-18 15:06:00 +0530

    Update shubham.c

--- shubham.c
+++ shubham.c
@@ -19,7 +19,7 @@ struct seller
  int pass=123;
 
  int main(void)
-{	
+	
     system("shieldofficial.mp3");
 char R[16]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
  system("color Ce");
